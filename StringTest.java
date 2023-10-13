8478f92809e649140f14fe47360a084962f9ee3d
riddhik18 <86958255+riddhik18@users.noreply.github.com>
2023-10-13 13:00:23 +0530

    StringTest.java

--- StringTest.java
+++ StringTest.java
@@ -2,10 +2,11 @@ package Java;
 import java.util.Scanner;
 public class StringTest {
     public static void main(String[] args) 
-    {
+{
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a  String: ");
         String input = scanner.nextLine();
+        
         String shiftedString = shiftCharacters(input);
         System.out.println("Shifted string: " + shiftedString);
     }
@@ -18,18 +19,18 @@ public class StringTest {
             {
                 char shiftedChar = (char) (((c - 'A' - 2)%26)+ 'A');
                 shiftedString.append(shiftedChar);
-            }
+             }
             else if (Character.isLowerCase(c)) 
-            {
+              {
                 char shiftedChar = (char) (((c - 'a' - 3)%26)+ 'a');
                 shiftedString.append(shiftedChar);
-            } 
+             } 
             else 
-            {
+              {
                 shiftedString.append(c);
-            }
-        }
+                }
+         }
         return shiftedString.toString();
-    }
+}
 }
 
